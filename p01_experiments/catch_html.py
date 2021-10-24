a15de2ad2e181509bca4d622b72b9090fd7190fd
YIZHEV <yizhev@gmail.com>
2021-10-24 16:22:13 +0800

    update Spider results.

--- p01_experiments/catch_html.py
+++ p01_experiments/catch_html.py
@@ -6,7 +6,7 @@ import requests
 
 
 # Define your web url, and simulate a web browser by defining the Header.
-url = "https://www.baidu.com"
+url = "http://www.baidu.com"
 headers = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'}
 
 # Check the HTTP response code.
@@ -15,9 +15,8 @@ print(f"The HTTP response is : {response.status_code}")
 
 # Get results, and write into file.
 results = urlopen(url)
-print(results.read().decode('utf-8'))
+# print(results.read().decode('utf-8'))
 # soup = BeautifulSoup(results, "html.parser")
-
 
 with open("spider_results.html", mode="w") as f:
     f.write(results.read().decode('utf-8'))
