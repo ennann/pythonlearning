1e23f49f5b46c64d98a64604250a814fb7cb909e
YIZHEV <yizhev@gmail.com>
2021-10-25 09:38:46 +0800

    Test spider Douban top 250.

---
+++ p01_experiments/web_request.py
@@ -0,0 +1,5 @@
+# Web process
+# 1. Process at the service : directly obtain the code from the service.
+# 2. Process at the client: firstly obtain the basic html code from the service, and download info again.
+# You can check it from Chrome => Network => Header.
+
