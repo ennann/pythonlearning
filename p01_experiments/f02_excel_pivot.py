7da5f765ef26a005e3c00e4487303a3a3ea5acc4
YIZHEV <yizhev@gmail.com>
2021-11-12 22:36:17 +0800

    made code more readable.

--- p01_experiments/f02_excel_pivot.py
+++ p01_experiments/f02_excel_pivot.py
@@ -3,17 +3,18 @@ from pandas import ExcelWriter
 
 
 # Read file and print to double check.
-print('-'*10+'Check the original table content'+'-'*10)
+print('-'*20+'Check the original table content.'+'-'*20)
 df = pd.read_excel(r"/Users/Elton/Downloads/服务台回收设备擦除记录11.11.xlsx", sheet_name="详情")
 print(df)
 
 # Export pivot table.
-print('-'*10+'Export pivot content.'+'-'*10)
-pivot_1 = pd.pivot_table(df, index=["工单类型", "工区"], values=["是否两小时", "是否擦除", "是否需要统计"], aggfunc="sum", margins=True).reset_index()
+print('-'*20+'Export pivot content.'+'-'*20)
+pivot_1 = pd.pivot_table(df, index=["工单类型", "工区"], values=["是否两小时", "是否擦除", "是否需要统计"],
+                         aggfunc="sum", margins=True).reset_index()
 
 # Export group.
-data_group = df.groupby(["备注",]).count()
-print(data_group)
+print('-'*20+'Export group content.'+'-'*20)
+data_group = df.groupby(["备注"]).count()
 
 
 # Write to Results file.
@@ -22,7 +23,7 @@ with ExcelWriter('/Users/Elton/Downloads/Results.xlsx') as writer:
     data_group.to_excel(writer, sheet_name="Sheet2")
 
 
-print('-'*10+'保存完成'+'-'*10)
+print('-'*20+'File saved.'+'-'*20)
 
 # Notes
 #
