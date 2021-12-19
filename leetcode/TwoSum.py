a7eb7fb039e6812bcbc9fce1ae72462ab15069db
YIZHEV <yizhev@gmail.com>
2021-12-19 21:10:50 +0800

    Rewrite DoubanTop250 program using XPath.

---
+++ leetcode/TwoSum.py
@@ -0,0 +1,15 @@
+
+nums = [2,7,11,15]
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return []
+
+print(Solution.twoSum(nums,9))
