859e7460a3a32fa2189db45b60d202c9c0f4fa1e
YIZHEV <yizhev@gmail.com>
2021-12-25 15:34:07 +0800

    add Netmusic

--- leetcode/isPalindrome.py
+++ leetcode/isPalindrome.py
@@ -3,3 +3,16 @@ class Solution:
         return str(x)[ : : -1] == str(x)
 
 print(Solution.isPalindrome(909))
+
+
+
+"""
+https://www.liaoxuefeng.com/wiki/1016959663602400/1017269965565856
+切片思想
+
+所有数，每5个取一个：
+>>> L[::5]
+
+
+
+"""
