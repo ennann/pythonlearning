8a7134bab4b7d5b1a6d48e0c3daf5708deaa582b
YIZHEV <yizhev@gmail.com>
2022-03-19 19:00:23 +0800

    Change File Directory.

--- p02_login/l01_douban cookies.py
+++ p02_login/l01_douban cookies.py
@@ -8,13 +8,19 @@ import time
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36",
 }
-
+cookies = {
+    "cookies": 'll="108288"; bid=xYgoyIf3tYc; _pk_id.100001.8cb4=80a89b087b8103ce.1647678267.1.1647678267.1647678267.; _pk_ses.100001.8cb4=*; __utma=30149280.8207973.1647678268.1647678268.1647678268.1; __utmc=30149280; __utmz=30149280.1647678268.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=30149280.1.10.1647678268; dbcl2="123168639:dNDG/PJYKso"'
+}
 
 url = "https://movie.douban.com/j/search_subjects?type=movie&tag=热门&sort=recommend&watched=on&page_limit=20&page_start=0"
-# url = "http://httpbin.org/get"
 
 response = requests.get(url, headers=headers, cookies=cookies)
-print(response.json())
+# print(response.json())
+
+results = response.json()
+
+print(results["subjects"][0]["title"])
+
 
 
 
