aef7abc3a5e2a74fd375a99bba081768907c43dc
YIZHEV <yizhev@gmail.com>
2022-03-19 18:51:07 +0800

    delete Cookies

--- p01_experiments/l01_douban cookies.py
+++ p01_experiments/l01_douban cookies.py
@@ -12,46 +12,53 @@ cookies = {
     "cookie": 'll="108288"; bid=xYgoyIf3tYc; _pk_id.100001.8cb4=80a89b087b8103ce.1647678267.1.1647678267.1647678267.; _pk_ses.100001.8cb4=*; __utma=30149280.8207973.1647678268.1647678268.1647678268.1; __utmc=30149280; __utmz=30149280.1647678268.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=30149280.1.10.1647678268; dbcl2="123168639:dNDG/PJYKso"'
 }
 
-url = 'https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=0'
+url = "https://movie.douban.com/j/search_subjects?type=movie&tag=热门&sort=recommend&watched=on&page_limit=20&page_start=0"
+# url = "http://httpbin.org/get"
 
 response = requests.get(url, headers=headers, cookies=cookies)
-
-source_code = response.text
-
-
-
-print(source_code)
-tree = etree.HTML(source_code)
-
-
-# results = tree.xpath("//*[@id='content']/div/div[1]/div/div[4]/div")
-"""
-XPath compare:
-//*[@id="content"]/div/div[1]/div/div[4]/div
-//*[@id="content"]/div/div[1]/div/div[4]/div/a[1]               第一个列表
-//*[@id="content"]/div/div[1]/div/div[4]/div/a[1]/p/text()      电影名字
-//*[@id="content"]/div/div[1]/div/div[4]/div/a[1]/div/img       电影封面
-//*[@id="content"]/div/div[1]/div/div[4]/div/a[2]               第二个
-
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]         第一个元素
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/p       电影名字
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/div/img 电影封面
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/p/strong 评分
-/html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[2]         第二个
+print(response.json())
 
 
 
-"""
 
+'''
+Totally wrong. This url returns a json file, but I still decode it by using HTML. 
+And when I running the Xpath, of course the XPath return null. 
+'''
 
-
-lists = tree.xpath('//div/div[4]/div/a')
-print(type(lists))
-print(len(lists))
-
-print(lists)
-
+#
+# source_code = response.text
+# tree = etree.HTML(source_code)
+#
+#
+# # results = tree.xpath("//*[@id='content']/div/div[1]/div/div[4]/div")
+# """
+# XPath compare:
+# //*[@id="content"]/div/div[1]/div/div[4]/div
+# //*[@id="content"]/div/div[1]/div/div[4]/div/a[1]               第一个列表
+# //*[@id="content"]/div/div[1]/div/div[4]/div/a[1]/p/text()      电影名字
+# //*[@id="content"]/div/div[1]/div/div[4]/div/a[1]/div/img       电影封面
+# //*[@id="content"]/div/div[1]/div/div[4]/div/a[2]               第二个
+#
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]         第一个元素
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/p       电影名字
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/div/img 电影封面
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[1]/p/strong 评分
+# /html/body/div[3]/div[1]/div/div[1]/div/div[4]/div/a[2]         第二个
+#
+#
+#
+# """
+#
+#
+#
+# lists = tree.xpath('//div/div[4]/div/a')
+# print(type(lists))
+# print(len(lists))
+#
+# print(lists)
+#
 
 time.sleep(0.1)
 
